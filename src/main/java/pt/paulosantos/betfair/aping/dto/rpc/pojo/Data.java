05548b1acc2269aefd5679b670b1e85be74bb5cc
Paulo Santos <email@paulosantos.pt>
2014-04-20 13:57:46 +0100

    Fixing data transfer objects for APINGException

--- src/main/java/pt/paulosantos/betfair/aping/dto/rpc/pojo/Data.java
+++ src/main/java/pt/paulosantos/betfair/aping/dto/rpc/pojo/Data.java
@@ -1,11 +1,17 @@
 package pt.paulosantos.betfair.aping.dto.rpc.pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import pt.paulosantos.betfair.aping.exceptions.APINGException;
 
 /**
  * Created by Paulo. 19-04-2014.
  */
 public class Data {
+
+    @JsonProperty("exceptionname")
+    private String exceptionName;
+
+    @JsonProperty("APINGException")
     private APINGException APINGException;
 
     public APINGException getAPINGException() {
@@ -14,5 +20,13 @@ public class Data {
 
     public void setAPINGException(APINGException aPINGException) {
         APINGException = aPINGException;
+    }
+
+    public String getExceptionName() {
+        return exceptionName;
+    }
+
+    public void setExceptionName(String exceptionName) {
+        this.exceptionName = exceptionName;
     }
 }
--- src/main/java/pt/paulosantos/betfair/aping/dto/rpc/pojo/Error.java
+++ src/main/java/pt/paulosantos/betfair/aping/dto/rpc/pojo/Error.java
@@ -4,6 +4,8 @@ package pt.paulosantos.betfair.aping.dto.rpc.pojo;
  * Created by Paulo. 19-04-2014.
  */
 public class Error {
+    private String message;
+    private Integer code;
     private Data data;
 
     public Data getData() {
@@ -12,5 +14,21 @@ public class Error {
 
     public void setData(Data data) {
         this.data = data;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
