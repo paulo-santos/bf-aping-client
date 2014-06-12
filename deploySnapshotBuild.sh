b1f73403dee2016a567e539b4f83cb9fecbe6022
Paulo Santos <email@paulosantos.pt>
2014-06-12 23:22:59 +0100

    Configuring travis to deploy snapshot builds to Sonatype Nexus

---
+++ deploySnapshotBuild.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo
+
+if [ "$TRAVIS_REPO_SLUG" != "paulo-santos/bf-aping-client"] && ["$TRAVIS_BRANCH" != "master"]  ; then
+  echo "WARNING: Not publishing snapshots as this is a branch" >&2
+  exit 1
+fi
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "WARNING: Not publishing snapshots as this is a pull request" >&2
+  exit 2
+fi
+
+mvn deploy -s upload_settings.xml -B
