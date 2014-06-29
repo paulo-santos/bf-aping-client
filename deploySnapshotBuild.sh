37258cc0141f62e7fee95932765c4b8ea8b72bea
Paulo Santos <email@paulosantos.pt>
2014-06-29 15:05:17 +0100

    Fixed credentials for artifact deployment Update deploy script to deploy the artifacts from oraclejdk7 build

--- deploySnapshotBuild.sh
+++ deploySnapshotBuild.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "JDK Version " $TRAVIS_JDK_VERSION
-
 if [ "$TRAVIS_REPO_SLUG" != "paulo-santos/bf-aping-client" ] && [ "$TRAVIS_BRANCH" != "master" ]  ; then
   echo "WARNING: Not publishing snapshots as this is a branch" >&2
   exit 1
@@ -10,6 +8,11 @@ fi
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo "WARNING: Not publishing snapshots as this is a pull request" >&2
   exit 2
+fi
+
+if [ "$TRAVIS_JDK_VERSION" != "oraclejdk7" ]; then
+    echo "WARNING: Not publishing this build as it will be depoyed by oraclejdk7 version build"
+    exit 3
 fi
 
 mvn deploy -s upload_settings.xml -B
